52dff0a02bcae5d5a90b617520e8c16a443dad6b
Rohini Mittal <rmittal@EX2INDIA.LOCAL>
2022-07-08 11:51:24 +0530

    initial setup

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+module.exports = {
+  mode: 'development',
+  entry: './src/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        include: [path.resolve(__dirname, "./src")],
+        use: 'ts-loader',
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  output: {
+    filename: 'build.js',
+    path: path.resolve(__dirname, 'public'),
+  },
+};
